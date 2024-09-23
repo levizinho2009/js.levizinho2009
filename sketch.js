b76404d30c0bed9e3efca0b2897c117c33053cc0
levizinho2009 <a.emerson@escola.pr.gov.br>
2024-09-23 10:28:39 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,28 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}let palavra;
+
+function setup() {
+  createCanvas(400, 400);
+
+  palavra = palavraAleatoria();
+}
+
+function palavraAleatoria(){
+  let palavras = ["Caminhante", "Caminho", "Caminha"];
+  return random(palavras);
+}
+
+function inicializaCores() {
+
+  background("white");
+  fill("black");
+  textSize(64);
+  textAlign(CENTER, CENTER);
+}
+
+function p
